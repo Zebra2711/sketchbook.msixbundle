a5e019b7469bfc8106421f51ecce5f616eac04db
Zebra2711 <89755535+Zebra2711@users.noreply.github.com>
2025-01-13 22:55:23 +0700

    add comment

--- install.cmd
+++ install.cmd
@@ -10,16 +10,21 @@ set "URL_SKETCHBOOK=https://github.com/Zebra2711/sketchbook.msixbundle/releases/
 if not exist "C:\Program Files\RunAsTI" mkdir "C:\Program Files\RunAsTI"
 
 cd "C:\Program Files\RunAsTI"
+
+:: Check OS
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32 || set OS=64
 
+:: Download RunAsTI
 if not exist "C:\Program Files\RunAsTI\RunAsTI%OS%.exe" powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/fafalone/RunAsTrustedInstaller/releases/download/v2.3.1/RunAsTI%OS%.exe', 'RunAsTI%OS%.exe')"
+
+:: Download and install Sketchbook
 powershell -Command "$path = [System.IO.Path]::Combine($env:TEMP, 'Sketchbook.SketchbookPro_9.1.38.0_neutral_~_k9x4nk31cvt0g.msixbundle');Write-Host 'Installation Path: ' $path;if (-Not (Test-Path $path)){Write-Host 'Downloading Sketchbook...';(New-Object Net.WebClient).DownloadFile('%URL_SKETCHBOOK%', $path);pause;} else {Write-Host 'File already exists.';}if (-Not (Test-Path $path)){Write-Host 'Download failed!'} else{Write-Host 'Install...';Add-AppxPackage -Path $path}"
 
 :: Fix warring when start app
 if not exist "C:\Windows\System32\config\systemprofile\Desktop" mkdir "C:\Windows\System32\config\systemprofile\Desktop"
 
-:: Download and install Sketchbook
-if (%OS% == 64){
+:: Create shortcut
+if ("%OS%" == "64"){
 	powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\Sketchbook Pro.lnk');$s.TargetPath='C:\Program Files\RunAsTI\RunAsTI64.exe';$s.IconLocation='C:\Program Files\WindowsApps\Sketchbook.SketchbookPro_9.1.38.0_x64__k9x4nk31cvt0g\SketchBookPro\SketchbookPro.exe';$s.Arguments='\"C:\Program Files\WindowsApps\Sketchbook.SketchbookPro_9.1.38.0_x64__k9x4nk31cvt0g\SketchBookPro\SketchbookPro.exe\"';$s.WorkingDirectory='C:\Program Files\WindowsApps\Sketchbook.SketchbookPro_9.1.38.0_x64__k9x4nk31cvt0g\SketchBookPro';$s.WindowStyle=7;$s.Save()"
 }else{
 	powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\Sketchbook Pro.lnk');$s.TargetPath='C:\Program Files\RunAsTI\RunAsTI32.exe';$s.IconLocation='C:\Program Files\WindowsApps\Sketchbook.SketchbookPro_9.1.38.0_x32__k9x4nk31cvt0g\SketchBookPro\SketchbookPro.exe';$s.Arguments='\"C:\Program Files\WindowsApps\Sketchbook.SketchbookPro_9.1.38.0_x32__k9x4nk31cvt0g\SketchBookPro\SketchbookPro.exe\"';$s.WorkingDirectory='C:\Program Files\WindowsApps\Sketchbook.SketchbookPro_9.1.38.0_x32__k9x4nk31cvt0g\SketchBookPro';$s.WindowStyle=7;$s.Save()"
